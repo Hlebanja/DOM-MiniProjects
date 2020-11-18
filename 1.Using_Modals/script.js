79b23ff5fa4d89dbbe62aa98f17a9844f1a4d44f
hlebanja <thiagohtoliva@gmail.com>
2020-11-18 06:49:20 +0100

    add pseudo-code

--- 1.Using_Modals/script.js
+++ 1.Using_Modals/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let modalButtons = document.querySelectorAll('.modal');
+let modalOverlay = document.querySelector('.overlay');
+let closeButton = document.querySelector('.close-modal');
+
+//create functions to remove and add "hidden" classes from .modal and .overlay
+//add event listener to modalButtons to use the functions
+//add event listener to closeButton, adding hidden to the modal and Overlay
+//add event listener to ESC and clicks outside the modal.
