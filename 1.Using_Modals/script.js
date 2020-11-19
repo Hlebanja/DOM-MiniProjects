493aba2a3b59a238bec4310ef8ad37e3ed84267c
hlebanja <thiagohtoliva@gmail.com>
2020-11-19 04:08:10 +0100

    add basic modal functionality

--- 1.Using_Modals/script.js
+++ 1.Using_Modals/script.js
@@ -1,10 +1,33 @@
 'use strict';
 
-let modalButtons = document.querySelectorAll('.modal');
+let modalButtons = document.querySelectorAll('.show-modal');
 let modalOverlay = document.querySelector('.overlay');
+let modalContent = document.querySelector('.modal');
 let closeButton = document.querySelector('.close-modal');
 
 //create functions to remove and add "hidden" classes from .modal and .overlay
 //add event listener to modalButtons to use the functions
 //add event listener to closeButton, adding hidden to the modal and Overlay
 //add event listener to ESC and clicks outside the modal.
+
+for (let i = 0; i < modalButtons.length; i++) {
+  modalButtons[i].addEventListener('click', showModal);
+}
+
+closeButton.addEventListener('click', hideModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modalOverlay.classList.contains('hidden')) {
+    hideModal();
+  }
+});
+
+function showModal() {
+  modalOverlay.classList.remove('hidden');
+  modalContent.classList.remove('hidden');
+}
+
+function hideModal() {
+  modalOverlay.classList.add('hidden');
+  modalContent.classList.add('hidden');
+}
